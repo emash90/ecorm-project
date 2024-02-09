3016ee986d152d9f3af058d4d94e65ed4e75a14a
edwin <edwin.macharia@gozem.co>
2024-02-09 16:34:12 +0300

    fixed order service port to use environment variable

--- backend/orderService/src/index.js
+++ backend/orderService/src/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 5003
 const cors = require('cors')
 const morgan = require('morgan')
+const dotenv = require('dotenv')
+dotenv.config()
 
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+const port = process.env.PORT || 5003
 const orderRoutes = require('./order-framework/express/routes/orderRoutes')
 
 app.use('/orders/v1/order', orderRoutes)
